d92b6e9bc552d167abca8019dcc15ff1b48c7066
Saloni Agrawal <s22agraw@edu.uwaterloo.ca>
2019-01-19 16:39:24 -0500

    TEMP: added background image

--- nutritionbar/userinterface/userinterface.py
+++ nutritionbar/userinterface/userinterface.py
@@ -1,4 +1,4 @@
-from guizero import App, Text, PushButton, Slider, info
+from guizero import App, Text, PushButton, Slider, info, Picture
 import csv
 
 valueList = [0, 0, 0]
@@ -47,27 +47,39 @@ def change_protein(slider_value):
 		valueList[2] = 2
 
 
-app = App(title="GranolaPod")
-welcome_message = Text(app, text="Select your macros", size=20, font="Dense", color="darkcyan")
+app = App(title="GranolaPod", layout="grid")
 
-fats = Text(app, text="Fats", size=18, font="Dense", color="black")
-fat_slider = Slider(app, command=change_fats, start=1, end=100)
+picture = Picture(app, image="bg.jpg", grid=[0,0,10,10])
+
+welcome_message = Text(app, text="Select your macros", size=20, font="Dense", color="darkcyan", grid=[2,0])
+welcome_message.tk.configure(background="grey")
+
+fats = Text(app, text="Fats", size=18, font="Dense", color="black", grid=[1,1])
+fats.tk.configure(background="grey")
+fat_slider = Slider(app, command=change_fats, start=1, end=100, grid=[2,1])
 fat_slider.width = 300
 fat_slider.height=30
+fat_slider.tk.configure(background="grey")
 
-carbs = Text(app, text="Carbs", size=18, font="Dense", color="black")
-carb_slider = Slider(app, command=change_carbs, start=1, end=100)
+carbs = Text(app, text="Carbs", size=18, font="Dense", color="black", grid=[1,2])
+carbs.tk.configure(background="grey")
+carb_slider = Slider(app, command=change_carbs, start=1, end=100, grid=[2,2])
 carb_slider.width = 300
 carb_slider.height = 30
+carb_slider.tk.configure(background="grey")
 
-protein = Text(app, text="Protein", size=18, font="Dense", color="black")
-protein_slider = Slider(app, command=change_protein, start=1, end=100)
+protein = Text(app, text="Protein", size=18, font="Dense", color="black", grid=[1,3])
+protein.tk.configure(background="grey")
+protein_slider = Slider(app, command=change_protein, start=1, end=100, grid=[2,3])
 protein_slider.width = 300
 protein_slider.height = 30
+protein_slider.tk.configure(background="grey")
 
-update_text = PushButton(app, command=generate_csv, text="Submit")
-update_text.width = 40
-update_text.height = 100
+update_text = PushButton(app, command=generate_csv, text="Submit", grid=[2,4])
+update_text.tk.configure(background="grey")
+#update_text.width = 30
+#update_text.height = 30
+
 app.display()
 
 for x in valueList:
