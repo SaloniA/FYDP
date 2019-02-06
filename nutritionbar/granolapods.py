d9d94701c2ac793057c9fb2ea14838e78f19de7f
Michael Kafarowski <michael.kafarowski@gmail.com>
2019-02-06 15:57:41 -0500

    Add webserver code and UI

---
+++ nutritionbar/granolapods.py
@@ -0,0 +1,6 @@
+def newRequestCallback(pval, fval, cval):
+	print("New Request Received:")
+	print("Protein: " +str(pval) + "%")
+	print("Fat: " +str(fval) + "%")
+	print("Carbs: " + str(cval) + "%")
+	#Insert code to execute when a new request is received.
---
+++ nutritionbar/server.py
@@ -0,0 +1,21 @@
+from bottle import route, run, static_file, template, request
+import granolapods
+import time
+
+@route('/', method=['GET','POST'])
+def index():
+	postdata = request.body.read()
+	if len(postdata) > 0:
+		pval = request.params.get('pval', type=int)
+		fval = request.params.get('fval', type=int)
+		cval = request.params.get('cval', type=int)
+		granolapods.newRequestCallback(pval, fval, cval)
+		time.sleep(5)
+	return template('./htdocs/index.html')
+
+@route('/static/<filename>')
+def server_static(filename):
+	return static_file(filename, root='./htdocs/')
+	
+if __name__ == '__main__':
+	run(reLoader=True, debug=True)
